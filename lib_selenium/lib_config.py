476bc66f0f7b2807adfddd2959a7260539e1e3dc
overflowy <overflowy@riseup.net>
2022-10-25 10:59:21 +0200

    lib_selenium: lib_config: Add support for environment variables

--- lib_selenium/lib_config.py
+++ lib_selenium/lib_config.py
@@ -1,41 +1,52 @@
 import os
+import re
 import sys
 from pathlib import Path
 
 import yaml
 
-VARS = {
-    "{{SCRIPT_DIR}}": Path(sys.argv[0]).parent,
-    "{{USERNAME}}": os.environ.get("USERNAME"),
-}
+EXPR = re.compile(r"{{(.*?)}}")
+os.environ["SCRIPT_DIR"] = str(Path(sys.argv[0]).parent)
 
 
-def replace_config_vars(config):
-    """Replace config vars with values from VARS."""
+def extract_text_between_double_curly_braces(text):
+    """Extract text between double curly braces."""
+    try:
+        return re.findall(EXPR, text)[0]
+    except (IndexError, TypeError):
+        return None
+
+
+def normalize_path(path):
+    """Normalize path."""
+    return str(Path(path).expanduser().resolve())
+
+
+def swap_env_vars(config):
+    """Swap environment variables with their values in config."""
     for section in config.keys():
         for key, value in config[section].items():
-            for var, replacement in VARS.items():
-                if not isinstance(value, str) or var not in value:
-                    continue
-                if isinstance(replacement, Path):
-                    value = value.replace(var, str(replacement))
-                    config[section][key] = str(Path(value))
-                else:
-                    config[section][key] = value.replace(var, replacement)
+            if not isinstance(value, str):
+                continue
+            if env_var := extract_text_between_double_curly_braces(value):
+                config[section][key] = value.replace("{{" + env_var + "}}", os.environ[env_var])
+                if "path" in key:
+                    config[section][key] = normalize_path(config[section][key])
     return config
 
 
 def get_config():
+    """Return config after swapping environment variables."""
     with open("config.yml") as f:
         config = yaml.safe_load(f)
-    return replace_config_vars(config)
+    return swap_env_vars(config)
 
 
 def get_logging_options(config):
     """Return logging options from config."""
-    path = config["Logging"]["path"]
-    level = config["Logging"]["level"]
-    log_exceptions = config["Logging"]["log_exceptions"]
-    display_stdout = config["Logging"]["display_stdout"]
-    mode = config["Logging"]["mode"]
+    path = config["Logging"].get("path")
+    level = config["Logging"].get("level", "info")
+    log_exceptions = config["Logging"].get("log_exceptions", True)
+    display_stdout = config["Logging"].get("display_stdout", True)
+    mode = config["Logging"].get("mode", "a")
     return path, level, log_exceptions, display_stdout, mode
