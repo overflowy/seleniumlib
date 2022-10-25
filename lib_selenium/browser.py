12a657d209379a67568d1bdc5310ea8b3b1f27a6
overflowy <overflowy@riseup.net>
2022-10-25 16:06:00 +0200

    lib_selenium: browser: Remove unused imports

--- lib_selenium/browser.py
+++ lib_selenium/browser.py
@@ -1,11 +1,6 @@
 import logging
 
 from selenium import webdriver
-from selenium.common.exceptions import *
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 
 from .lib_config import get_config
