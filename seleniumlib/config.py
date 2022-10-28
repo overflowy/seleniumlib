d80c5d5a7fd89eecdd5943c6c839fd79950320a5
overflowy <overflowy@riseup.net>
2022-10-28 16:41:01 +0200

    seleniumlib: config: Refactor functions

--- seleniumlib/config.py
+++ seleniumlib/config.py
@@ -1,18 +1,18 @@
+import json
 import os
 import re
 import sys
 from pathlib import Path
 
-import json
 
-EXPR = re.compile(r"{{(.*?)}}")
+ENV_VAR_EXPR = re.compile(r"{{(.*?)}}")
 os.environ["SCRIPT_DIR"] = str(Path(sys.argv[0]).parent)
 
 
 def extract_text_between_double_curly_braces(text):
-    """Extract text between double curly braces."""
+    """Extract environment variable between double curly braces."""
     try:
-        return re.findall(EXPR, text)[0]
+        return re.findall(ENV_VAR_EXPR, text)[0]
     except IndexError:
         return None
 
@@ -23,24 +23,37 @@ def normalize_path(path, as_str=True):
     return str(n_path) if as_str else n_path
 
 
-def swap_env_vars(config):
-    """Swap environment variables with their values in config."""
+def expand_env_vars(config):
+    """Expand environment variables in config if they have been defined."""
     for section in config.keys():
         for key, value in config[section].items():
             if not isinstance(value, str):
                 continue
-            if env_var := extract_text_between_double_curly_braces(value):
+            if env_var := extract_text_between_double_curly_braces(value) and os.environ.get(env_var):
                 config[section][key] = value.replace("{{" + env_var + "}}", os.environ[env_var])
                 if "path" in key:
                     config[section][key] = normalize_path(config[section][key])
     return config
 
 
+def try_open_config_file(config_file):
+    """Try to open config file."""
+    try:
+        with open(config_file) as f:
+            return json.load(f)
+    except FileNotFoundError:
+        print(f"Config file {config_file} not found.")
+        sys.exit(1)
+
+
 def get_config():
-    """Return config after swapping environment variables."""
-    with open("config.json") as f:
-        config = json.load(f)
-    return swap_env_vars(config)
+    """Return config after expanding environment variables.
+    If SELENIUMLIB_CFG environment variable is defined, use that as config file."""
+    if seleniumlib_cfg := os.environ.get("SELENIUMLIB_CFG"):
+        config = try_open_config_file(seleniumlib_cfg)
+    else:
+        config = try_open_config_file("config.json")
+    return expand_env_vars(config)
 
 
 def get_logging_options(config):
