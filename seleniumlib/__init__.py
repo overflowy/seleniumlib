46bbbff86fee7282471d2f595bb497b6e9d69b07
overflowy <overflowy@riseup.net>
2022-11-07 17:33:08 +0100

    seleniumlib: Remove unused import

--- seleniumlib/__init__.py
+++ seleniumlib/__init__.py
@@ -8,7 +8,6 @@ from pathlib import Path
 
 from selenium.common.exceptions import *
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
